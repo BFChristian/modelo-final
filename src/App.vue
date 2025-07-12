418a03af54109442257b0871fe9a689b8bd5d9f3
Christian <b.christian.fuentes@gmail.com>
2025-07-12 01:27:31 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Conversor />
+</template>
+
+<script>
+import Conversor from './components/Conversor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Conversor
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Conversor.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1>Conversor de temperatura</h1>
+    <label for="celcius">Ingrese Temperatura: °C </label>
+    <input id="celcius" type="text" v-model="celcius" />
+    <p for="">Conv Temperatura °F: </p>
+    <p for="">Conv Temperatura °K: </p>
+</template>
+
+<script>
+
+export default {
+    name: 'Conversor',
+    data() {
+        return {
+            celcius: '',
+            fahrenheit: '',
+            kelvin: ''
+        };
+    },
+    methods: {
+        // Aquí puedes agregar métodos para manejar la lógica de conversión
+
+    },
+    computed: {
+        // Aquí puedes agregar propiedades computadas si es necesario
+    },
+    mounted() {
+        // Aquí puedes inicializar cualquier dato o realizar acciones al montar el componente
+    }
+}
+
+</script>
+
+<style scoped></style>
