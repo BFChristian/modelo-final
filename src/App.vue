d4dc2b6546fd91f9a50bd995a57e45aab77d2fef
Christian <b.christian.fuentes@gmail.com>
2025-07-13 17:45:43 -0300

    terminado

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <Conversor />
+  <h1>Respuestas:</h1>
+  <p>1:c</p>
+  <p>2:b</p>
+  <p>3:c</p>
+  <p>4:a</p>
 </template>
 
 <script>
--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -1,9 +1,9 @@
 <template>
     <h1>Conversor de temperatura</h1>
     <label for="celcius">Ingrese Temperatura: °C </label>
-    <input id="celcius" type="text" v-model="celcius" />
-    <p for="">Conv Temperatura °F: </p>
-    <p for="">Conv Temperatura °K: </p>
+    <input id="celcius" type="text" v-model.number="celcius" />
+    <p :class="selectorDeColor(fahrenheit)">Conv Temperatura °F: {{ fahrenheit }}</p>
+    <p :class="selectorDeColor(kelvin)">Conv Temperatura °K: {{ kelvin }}</p>
 </template>
 
 <script>
@@ -12,30 +12,39 @@ export default {
     name: 'Conversor',
     data() {
         return {
-            celcius: '',
-            fahrenheit: '',
-            kelvin: ''
+            celcius: 0,
         };
     },
     methods: {
         // Aquí puedes agregar métodos para manejar la lógica de conversión
         convertirCelciusAFahrenheit() {
-            if (this.celcius !== '') {
-                this.fahrenheit = (this.celcius * 9/5) + 32;
+            if (this.celcius !== 0) {
+                return (this.celcius * 9 / 5) + 32;
             } else {
-                this.fahrenheit = '';
+                return 0;
             }
         },
         convertirCelciusAKelvin() {
-            if (this.celcius !== '') {
-                this.kelvin = parseFloat(this.celcius) + 273.15;
+            if (this.celcius !== 0) {
+                return parseFloat(this.celcius) + 273.15;
             } else {
-                this.kelvin = '';
+                return 0;
             }
+        },
+        selectorDeColor(grados) {
+            if (grados <= 0) return 'azul';
+            if (grados < 15) return 'magenta';
+            return 'rojo';
         }
     },
     computed: {
         // Aquí puedes agregar propiedades computadas si es necesario
+        kelvin() {
+            return this.convertirCelciusAKelvin();
+        },
+        fahrenheit() {
+            return this.convertirCelciusAFahrenheit();
+        }
     },
     mounted() {
         // Aquí puedes inicializar cualquier dato o realizar acciones al montar el componente
@@ -44,4 +53,16 @@ export default {
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.azul {
+    color: blue;
+}
+
+.magenta {
+    color: magenta;
+}
+
+.rojo {
+    color: red;
+}
+</style>
